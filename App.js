6e0884ddc50f4a4b3932acbf54a78ca25580973a
Your Name <geethajyothi2000@gmail.com>
2024-09-06 08:44:24 +0530

    gaadi_project

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { usePushNotifications } from './src/service/notificationservice';
 
 export default function App() {
+  const {expoPushToken ,notification} = usePushNotifications([])
+   
+const [expoToken, setExpoToken] = useState("no token");
+useEffect(() => {
+  if (expoPushToken) {
+    setExpoToken(expoPushToken.data); // Ensure this is the correct path.
+  }
+}, [expoPushToken]);
+console.log(expoToken)
+console.log(notification)
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
